e458f7ff74042a7e5ec5c0bfd1beb9b6d63275ce
matiasangelani <matiasangelani2@gmail.com>
2022-06-09 11:11:39 -0300

    Encolar proceso, modificar prioridad y listar todos los procesos agregados. Error de ingreso de string capturado.

--- sintaxis/TP TAD/app/index.py
+++ sintaxis/TP TAD/app/index.py
@@ -20,14 +20,32 @@ Lista de TADs
 #Importo TAD
 #import tads.tadProcesos
 
+from os import system
+from time import sleep
 from tads.processesTAD import *
 
 def menu():
+  system('clear')
   print('\n\n', format(' SELECCIONAR UNA OPCION ', '=^50'))
-  print('\n1. Encolar Proceso')
+  print('\n1. Encolar proceso')
+  print('\n2. Modificar prioridad de proceso')
+  print('\n3. Desencolar proceso')
+  print('\n4. Listar todos procesos')
+  print('\n5. Listar un proceso')
+  print('\n6. Cambiar a prioridad baja todos los procesos dado un mes')
+  print('\n7. Eliminar todos los procesos de un tipo particular')
+  print('\n8. Generar nueva cola a partir de intervalo horario')
+
   print('\n',format(' Presione 0 para finalizar ', '*^40'))
 
-  option = int(input('Opcion: '))
+  #Modificar porque si ingreso número decimal se rompe
+  #option = int(input('Opcion: '))
+  optionInput = input('\nOpcion: ')
+
+  try:
+    option = int(optionInput)
+  except ValueError:
+    option = -1
 
   return option
 
@@ -36,29 +54,65 @@ queue = createQueue()
 option = menu()
 
 while option != 0:
-  # if option == 0:
-  #   print('\nPrograma terminado')
-  if option < 1 or option > 7:
-    print('\nLa opcion ingresada es incorrecta, vuelva a intentarlo')
+  if option < 1 or option > 8:
+    print('\n\nLa opcion ingresada es incorrecta, vuelva a intentarlo')
+    print('\n\n------------------------------------------------------')
+    print('\n\nCLEARING...')
+    sleep(1.5)
     option = menu()
   else:
+    if option == 1:
+      process = createProcess()
+      name = input('\nIngresar nombre: ')
+      processType = input('Ingresar tipo: ')
+      size = input('Ingresar tamaño: ')
+      priority = input('Ingresar prioridad: ')
+
+      response = addProcess(queue, process, name, processType, size, priority)
+
+      print('\n',response)
+      sleep(0.5)
+    elif option == 2:
+      index = int(input('\nIngresar indice de proceso: '))
+      search = searchProcess(queue, index)
+      if search:
+        print('\n1. LOW')
+        print('2. MID')
+        print('3. HIGH')
+        inputPriority = input('\nSeleccionar prioridad: ')
+
+        try:
+          priority = int(inputPriority)
+        except ValueError:
+          priority = -1
+
+        response = modPriority(queue, index, priority)
+
+        print(response)
+      else:
+        print('\nEl proceso no existe')
+
+    elif option == 12:
+      index = int(input('Ingresar indice de proceso: '))
+      search = searchProcess(queue, index)
+      if search:
+        name = input('Ingresar nombre: ')
+        processType = input('Ingresar tipo: ')
+        size = input('Ingresar tamaño: ')
+        priority = input('Ingresar prioridad: ')
+
+        response = modProcess(queue, index, name, processType, size, priority)
+
+        print(response)
+      else:
+        print('\nEl proceso no existe')
+    elif option == 4:
+      listAllProcess(queue)
+      input('\n\nPresionar enter para mostrar menú')
+
     option = menu()
 
+
+
+
 print('\nPrograma terminado')
-
-#print('Message: ', queue)
-
-#Menu
-
-
-
-
-
-# name = input('Input process name: ')
-# type = input('Input process type: ')
-# size = input('Input process size: ')
-# priority = input('Input process priority: ')
-# date = input('Input process date: ')
-# hour = input('Input process hour: ')
-
-
