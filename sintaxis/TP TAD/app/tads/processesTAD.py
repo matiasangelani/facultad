2057985e3dd56b232036803168da3c2c22ff7da4
matiasangelani <matiasangelani2@gmail.com>
2022-06-09 11:22:27 -0300

    createQueue, createProcess, addProcess, listAllProcess, modProcess, delProcess, searchProcess, modPriority implementados en processes TAD

--- sintaxis/TP TAD/app/tads/processesTAD.py
+++ sintaxis/TP TAD/app/tads/processesTAD.py
@@ -1,14 +1,66 @@
+from datetime import datetime
+
 def createQueue():
   #Crea una cola
-  return 'Queue created'
+  queue = []
+  return queue
 
-def addProcess(name, type, size, priority, date, hour):
+def createProcess():
+  #Crea un proceso
+  process = ["","","","",""]
+  return process
+
+def addProcess(queue, process, name, processType, size, priority):
   #Agrega proceso a la cola
+  process[0] = name
+  process[1] = processType
+  process[2] = size
+  process[3] = priority
+  process[4] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+  queue.append(process)
+
   return 'Process added'
 
-def modProcess(name, type, size, priority, date, hour):
+def listAllProcess(queue):
+  print ('\n\t{:<5} {:<10} {:<10} {:<10} {:<10} {:<10}'.format('#','Nombre','Tipo', 'Tamaño', 'Prioridad', 'Fecha'))
+  for i, process in enumerate(queue):
+    name, processType, size, priority, date = process
+    print('\t{:<5} {:<10} {:<10} {:<10} {:<10} {:<10}'.format(i, name, processType, size, priority, date))
+
+def modProcess(queue, index, name, processType, size, priority):
   #Modifica un proceso
+  for i, process in enumerate(queue):
+    if index == i:      
+      process[0] = name
+      process[1] = processType
+      process[2] = size
+      process[3] = priority
+      process[4] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
   return 'Process modified'
 
-def delProcess(name):
+def delProcess(queue):
   return 'Process deleted'
+
+def searchProcess(queue, index):
+  for i, process in enumerate(queue):
+    if i == index:
+      return True
+
+  return False
+
+def modPriority(queue, index, priority):
+  #Modifica prioridad de un proceso
+  if priority == 1:
+    priority = 'LOW'
+  elif priority == 2:
+    priority = 'MID'
+  elif priority == 3:
+    priority = 'HIGH'
+
+  for i, process in enumerate(queue):
+    if index == i:      
+      process[3] = priority
+
+  return 'Priority modified'
